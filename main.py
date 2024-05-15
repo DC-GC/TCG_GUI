a528e29987d1b57480c03fc14e4a94b54bcfe48e
Dan Carlos Gabriel Medilla <dan.medilla@umtc.com.ph>
2024-05-15 12:59:21 +0800

    Add basic box layout to project.

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+class BoxLayoutSample(BoxLayout):
+    pass
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #     button1 = Button(text= "button1")
+    #     button2 = Button(text= "button2")
+    #     self.add_widget(button1)
+    #     self.add_widget(button2)
+    #     self.orientation = "vertical"
 
 class MainWidget(Widget):
     pass
