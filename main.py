26b215b490c03c8101d7a6fecca7c852b8d3ddf6
Dan Carlos Gabriel Medilla <dan.medilla@umtc.com.ph>
2024-05-16 16:29:34 +0800

    Update display.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 
